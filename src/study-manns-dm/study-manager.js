574757087161e10fbd987e6b2ea678aa2ba30058
drishya-nair <nair.drish@gmail.com>
2024-05-02 01:18:03 -0700

    fixed font sizing for manns dm results page

--- src/study-manns-dm/study-manager.js
+++ src/study-manns-dm/study-manager.js
@@ -16,7 +16,7 @@ require("jquery-ui-bundle");
 var _ = require('lodash');
 var introTemplate = require("../templates/introduction.html");
 var irbTemplate = require("../templates/irb.html");
-var demographicsTemplate = require("../templates/demographics.html");
+var demographicsTemplate = require("./templates/demographics.html");
 var question1Template = require("./templates/decision-making.html");
 var loadingTemplate = require("../templates/loading.html");
 var resultsTemplate = require("./templates/results.html");
@@ -37,7 +37,7 @@ module.exports = (function(exports) {
 		study_recommendation: [],
 		preLoad: ["../img/btn-next.png","../img/btn-next-active.png","../img/ajax-loader.gif"],
 		slides: {
-			INTRODUCTION: {
+			/*INTRODUCTION: {
 				name: "introduction",
 				type: "display-slide",
 				template: introTemplate,
@@ -60,7 +60,7 @@ module.exports = (function(exports) {
 					var dem_data = $('#demographicsForm').alpaca().getValue();
 					LITW.data.submitDemographics(dem_data);
 				}
-			},
+			},*/
 			QUESTION1: {
 				name: "questionnaire",
 				type: "display-slide",
@@ -118,9 +118,9 @@ module.exports = (function(exports) {
 
 	function configureStudy() {
 		params.questionOrderArray = randomizeArray(createArray());
-		timeline.push(params.slides.INTRODUCTION);
+		/*timeline.push(params.slides.INTRODUCTION);
 		timeline.push(params.slides.INFORMED_CONSENT);
-		timeline.push(params.slides.DEMOGRAPHICS);
+		timeline.push(params.slides.DEMOGRAPHICS);*/
 		timeline.push(params.slides.QUESTION1);
 		timeline.push(params.slides.QUESTION2);
 		timeline.push(params.slides.QUESTION3);
